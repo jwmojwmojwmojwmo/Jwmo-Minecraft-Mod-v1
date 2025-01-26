451a8f80303d997d876bd4a256999617109f78f3
Jason Wang <jwmo@students.cs.ubc.ca>
2025-01-25 23:21:40 -0800

    Initial commit

---
+++ src/client/java/v1/jwmomod/JwmomodClient.java
@@ -0,0 +1,10 @@
+package v1.jwmomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JwmomodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
