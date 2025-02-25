06a998ae19fad37cc5cec86dca4fd16211d988c5
Jason Wang <jwmo@students.cs.ubc.ca>
2025-02-24 20:23:44 -0800

    Fixed Wolf transformer block

--- src/main/java/v1/jwmomod/block/custom/JwmoWolfTransformerBlock.java
+++ src/main/java/v1/jwmomod/block/custom/JwmoWolfTransformerBlock.java
@@ -10,6 +10,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class JwmoWolfTransformerBlock extends Block {
@@ -21,13 +22,16 @@ public class JwmoWolfTransformerBlock extends Block {
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if (!world.isClient()) {
             if (entity instanceof MobEntity mobentity && entity.getType() != EntityType.WOLF) {
-                mobentity.teleport(pos.getX() + 5, pos.getY(), pos.getZ(), false);
+                PlayerEntity player = world.getClosestPlayer(mobentity, 30d);
+                mobentity.teleport(pos.getX() + 100, 100, pos.getZ() + 100, false);
                 WolfEntity wolf = new WolfEntity(EntityType.WOLF, world);
-                PlayerEntity player = world.getClosestPlayer(entity, 20d);
-                wolf.setOwner(player);
-                wolf.setPosition(player.getPos().offset(Direction.NORTH, 3));
-                mobentity.kill((ServerWorld) world);
+                wolf.setPosition(Vec3d.of(pos.offset(Direction.NORTH, 2)));
+                if (!(player == null)) {
+                    wolf.setOwner(player);
+                    wolf.setPosition(player.getPos().offset(Direction.NORTH, 3));
+                }
                 world.spawnEntity(wolf);
+                mobentity.kill((ServerWorld) world);
             }
         }
     }
